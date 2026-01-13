9c4250bfffa1575b8b4442dc67fbe5de05300637
Tboiii-123 <lawalhussein775@gmail.com>
2026-01-13 14:49:18 +0100

    commited

--- logistics/views.py
+++ logistics/views.py
@@ -8,7 +8,6 @@ from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework_simplejwt.tokens import RefreshToken
 
 # Local app imports
